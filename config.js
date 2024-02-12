8228490030f488ab6bb91c429006d826f0424e4e
Uuyahz01 <110936334+Uuyahz01@users.noreply.github.com>
2024-02-13 01:34:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Đậu dành tặng Thảo",
-    introTitle: 'Thảo à',
+    titleWeb: "Duyyy dành tặng Vyyy",
+    introTitle: 'Vyyy à',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '💗 Click 💗',
+    btnIntro: '💗 Bấm Vào Đây Để Tới Trái Tim Anh 💗',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Yêu lắm 💝',
     btnNo: 'Không nha 😂',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao Thảo lại yêu mình 🤣',
-    btnReply: '💖 Gửi cho Đậu 💖',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Vyyy lại yêu mình 🤣',
+    btnReply: '💖 Gửi cho TQDuyy 💖',
     reply: 'Yêu thì yêu mà không yêu thì yêu 😋',
-    mess: 'Đậu biết mà 🥰. Yêu Thảo nhiều nhiều 😘😘',
+    mess: 'Duyy biết mà 🥰. Yêu Vyyy nhiều nhiều 😘😘',
     messDesc: 'Anh hứa , sau này có tiền anh sẽ nuôi em thành HEO.',
     btnAccept: 'Okiiiii lun 🥰',
-    messLink: 'https://github.com/DauDau432/Confess-Crush' //link mess của các bạn.
+    messLink: 'https://www.facebook.com/profile.php?id=1124333038' //link mess của các bạn.
 }
